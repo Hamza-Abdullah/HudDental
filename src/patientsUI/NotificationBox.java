dd24bef8eff8775b433fce8928eb88bba28f96c5
U1856364 <christianharborow@hotmail.co.uk>
2019-12-09 16:15:42 +0000

    Added notification page and ability to switch between pages

---
+++ src/patientsUI/NotificationBox.java
@@ -0,0 +1,34 @@
+package patientsUI;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
+public class NotificationBox extends VBox {
+    private Label timeLabel;
+    private Label notificationLabel;
+
+    public NotificationBox() {
+        this.setStyle("-fx-background-color: #80CEE1");
+        this.setFillWidth(false);
+        this.setPadding(new Insets(10, 10, 10, 10));
+
+        this.timeLabel = new Label();
+        this.timeLabel.setFont(new Font("Arial Black", 15));
+
+        this.notificationLabel = new Label();
+        this.notificationLabel.setFont(new Font("Arial Black", 20));
+
+        this.getChildren().addAll(timeLabel, notificationLabel);
+    }
+
+    public void setTimeLabel(String string){
+        this.timeLabel.setText(string);
+    }
+
+    public void setNotificationLabel(String string){
+        this.notificationLabel.setText(string);
+    }
+
+}
--- src/patientsUI/PatientNotificationsUI.java
+++ src/patientsUI/PatientNotificationsUI.java
@@ -1,9 +1,34 @@
 package patientsUI;
 
+import javafx.geometry.Insets;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class PatientNotificationsUI extends PatientBaseUI{
-    public PatientNotificationsUI(Stage primaryStage) {
-        super(primaryStage);
+    private VBox notificationVBox;
+
+    public PatientNotificationsUI(Stage primaryStage, AppointmentUIMain controller) {
+        super(primaryStage, controller);
+        setTitle("Your Notifications");
+
+        notificationVBox = new VBox();
+        notificationVBox.setPadding(new Insets(40, 40, 40, 40));
+        notificationVBox.setSpacing(20);
+        notificationVBox.setFillWidth(false);
+        getMainVBox().getChildren().add(notificationVBox);
+
+        //get notifications from database
+        testData();
     }
+
+    public void testData(){
+        //temporary function
+        for (int i = 0; i <= 7; i++){
+            NotificationBox nb = new NotificationBox();
+            nb.setTimeLabel("2hrs ago");
+            nb.setNotificationLabel("Appointment is tomorrow at 2:30");
+            notificationVBox.getChildren().add(nb);
+        }
+    }
+
 }
